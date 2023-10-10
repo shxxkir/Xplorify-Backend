7e05bef290ba10cfa89d280a75f76d0c244338dd
shxxkir <128359891+shxxkir@users.noreply.github.com>
2023-10-10 15:52:28 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./configs/db");
+
+const app = express();
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+// Parse JSON request bodies
+app.use(express.json());
+
+const PORT = process.env.PORT || 8080;
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
